d514b3012795ddf7a1b7865853ff15b138969de0
Manuel Hutter <manuel@hutter.io>
2018-12-27 18:54:42 +0100

    Configure webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path')
 
+const publicDir = path.join(__dirname, 'public')
+
 module.exports = {
   mode: 'development',
   entry: './src/app.js',
   output: {
-    path: path.join(__dirname, 'public'),
+    path: publicDir,
     filename: 'bundle.js'
   },
   module: {
@@ -12,5 +14,8 @@ module.exports = {
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
     ]
   },
-  devtool: 'cheap-module-eval-source-map'
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: publicDir
+  }
 }
